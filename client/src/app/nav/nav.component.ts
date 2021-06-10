ea4bc296872df4739f60ad5acedafc5d561f57db
darthleonard <leolinuxmx@gmail.com>
2021-06-09 23:21:05 -0500

    66. Routing in code

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { User } from '../models/user';
+import { Router } from '@angular/router';
 import { AccountService } from '../services/account.service';
 
 @Component({
@@ -11,14 +10,14 @@ import { AccountService } from '../services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(resp => {
-      console.log(resp);
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
     });
@@ -26,5 +25,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
