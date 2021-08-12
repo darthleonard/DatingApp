d162b6e771684d8966099f84b9ade1466acef381
darthleonard <leolinuxmx@gmail.com>
2021-08-11 23:16:21 -0500

    91. Creating a repository

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private DataContext context { get; }
+
+        public UserRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            return await context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await context.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
